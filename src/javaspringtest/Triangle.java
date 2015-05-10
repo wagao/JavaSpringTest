1deef677dafbcba331df9b51ef9e2cfd14a54ba3
Wa Gao <wgao@stevens.edu>
2015-05-10 17:50:01 -0600

    bean scope, contextAware

--- src/javaspringtest/Triangle.java
+++ src/javaspringtest/Triangle.java
@@ -6,14 +6,31 @@
 package javaspringtest;
 
 import java.util.List;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  *
  * @author Emma
  */
-public class Triangle {
+public class Triangle implements ApplicationContextAware, BeanNameAware{
     private List<Point> points;
+    private ApplicationContext context=null;
 
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException { 
+        this.context=context;
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("this is beanName:"+beanName);
+//        this is executed before the obj
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     public List<Point> getPoints() {
         return points;
     }
